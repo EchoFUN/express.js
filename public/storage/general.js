e3f00f8066f23484bfd799d471f2294632bf6485
bjxukai <bjxukai@corp.netease.com>
2013-11-18 09:58:22 +0800

    COMMIT

--- public/storage/general.js
+++ public/storage/general.js
@@ -4,11 +4,12 @@
  * @author KEN.
  */
 
-var Storage;
+var Storage = LS;
 
 var targetMethod = function(data) {
 
    // TODO 处理准备好的逻辑。
+   console.log(data);
 };
 
 var storageChangeHandler = function(data) {
@@ -18,8 +19,8 @@ var storageChangeHandler = function(data) {
       }
       targetMethod($.parseJSON(data));
    } else {
-      if (data.key == 'users') {getItem
-         if (data.newValue == '') {
+      if (data.key == 'users') {
+         if (!data.newValue) {
             return;
          }
          targetMethod($.parseJSON(data.newValue));
@@ -27,7 +28,7 @@ var storageChangeHandler = function(data) {
    }
 };
 
-if (window.localStorage && window.localStorage.) {
+if (window.localStorage && window.localStorage.setItem) {
    window.addEventListener('storage', storageChangeHandler, false);
 } else {
    var oriUsers = Storage.get('users');
@@ -40,3 +41,31 @@ if (window.localStorage && window.localStorage.) {
       }
    }, 1000)
 }
+
+document.addEventListener('click', function() {
+   Storage.set('users', '{"a":"test"}');
+});
+
+/* ---------------------------- 转换成string --------------------------- */
+
+function stringify(obj) {
+   if ('JSON' in window) {
+      return JSON.stringify(obj);
+   }
+
+   var n, v, json = [], arr = (obj && obj.constructor == Array);
+   for (n in obj) {
+      v = obj[n];
+      var t = typeof (v);
+      if (obj.hasOwnProperty(n)) {
+         if (t == 'string') {
+            v = '"' + v + '"';
+         } else if (t == 'object' && v !== null) {
+            v = stringify(v);
+         }
+         json.push(( arr ? "" : '"' + n + '":') + String(v));
+      }
+   }
+   return ( arr ? "[" : "{") + String(json) + ( arr ? "]" : "}");
+}
+
