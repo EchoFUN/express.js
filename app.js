806273d7e3a0b5d6c90cdf4319fda2912a3a360c
bjxukai <bjxukai@corp.netease.com>
2013-11-18 10:44:07 +0800

    修改请求的logger格式。

--- app.js
+++ app.js
@@ -2,32 +2,38 @@
  * Module dependencies.
  */
 
-process.env.DEBUG = true;
-
 var express = require('express'), http = require('http'), path = require('path');
 
 var map = require('./map');
 
 var app = express();
 
+var logger = express.logger(':remote-addr : visite the host(:date)');
+
 app.configure(function() {
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+   app.use(function(req, res, next) {
+      if (!(/\.(png|jpg|gif|jpeg)$/i).test(req.path)) {
+         logger(req, res, next);
+      } else {
+         next();
+      }
+   });
+   app.set('port', process.env.PORT || 3000);
+   app.set('views', __dirname + '/views');
+   app.set('view engine', 'ejs');
+   app.use(express.favicon());
+   app.use(express.bodyParser());
+   app.use(express.methodOverride());
+   app.use(express.static(path.join(__dirname, 'public')));
+   app.use(app.router);
 });
 
 app.configure('development', function() {
-  app.use(express.errorHandler());
+   app.use(express.errorHandler());
 });
 
 map(app);
 
 http.createServer(app).listen(app.get('port'), function() {
-  console.log("Express server listening on port " + app.get('port'));
+   console.log("Express server listening on port " + app.get('port'));
 });
